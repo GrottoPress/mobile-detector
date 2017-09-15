360baf072f701f764ae986b36efe19d2b5c04ac7
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2017-09-15 21:42:48 +0000

    initial commit

---
+++ tests/unit/DetectorTest.php
@@ -0,0 +1,57 @@
+<?php
+declare (strict_types = 1);
+
+namespace GrottoPress\Mobile;
+
+use Codeception\Test\Unit;
+
+class DetectorTest extends Unit
+{
+    /**
+     * @var Detector
+     */
+    protected $detector;
+
+    protected function _before()
+    {
+        $this->detector = new Detector();
+    }
+
+    public function testGetBrowser()
+    {
+        $this->detector->setUserAgent($this->detector->getBrowsers()['Chrome']);
+
+        $this->assertSame($this->detector->getBrowser(), 'Chrome');
+    }
+
+    public function testGetOperatingSystem()
+    {
+        $this->detector->setUserAgent(
+            $this->detector->getOperatingSystems()['AndroidOS']
+        );
+
+        $this->assertSame($this->detector->getOperatingSystem(), 'AndroidOS');
+    }
+
+    public function testGetDevice()
+    {
+        $this->detector->setUserAgent(
+            $this->detector->getDevices()['AinolTablet']
+        );
+
+        $this->assertSame($this->detector->getDevice(), 'AinolTablet');
+    }
+
+    public function testIsPhone()
+    {
+        $this->detector->setUserAgent(
+            $this->detector->getDevices()['iJoyTablet']
+        );
+        $this->assertFalse($this->detector->isPhone());
+
+        $this->detector->setUserAgent(
+            $this->detector->getPhoneDevices()['Pantech']
+        );
+        $this->assertTrue($this->detector->isPhone());
+    }
+}
